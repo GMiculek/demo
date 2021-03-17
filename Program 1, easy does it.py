32f5a1aeb0a94b22780a6ae57043f7f3bafbc09f
GMiculek <80844562+GMiculek@users.noreply.github.com>
2021-03-17 11:35:28 -0500

    Add files via upload
    Adding a simple program

---
+++ Program 1, easy does it.py
@@ -0,0 +1,14 @@
+###########################################################################################
+# Name: Garrett Miculek                                                                   #
+# Date: 9-30-19                                                                           #
+# Description: Asking for a name and an age.                                              #
+###########################################################################################
+
+# prompt the user for a name and an age
+name = input("Please enter your name: ")
+print(name)
+age = input("How old are you, " + name + "? ")
+print(age)
+# display the final output
+twice =(int(age)*int(2))
+print("Hi, " + name + "." " You are " + age + " years old. Twice your age is " + str(twice) + ".")
